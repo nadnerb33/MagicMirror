024e7b2c5cd3eef744f57218acb53a91d9cdbf5c
nadnerb33 <brendan@brendans-mbp.home>
2016-05-02 12:54:03 +0100

    started trains module

---
+++ modules/default/trains/trains.js
@@ -0,0 +1,23 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: HelloWorld
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+Module.register("trains",{
+
+	// Default module config.
+	defaults: {
+		text: "Next trains"
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.text;
+		return wrapper;
+	}
+});
